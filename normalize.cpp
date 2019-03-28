7a5c658ac0b4595e526908570653bd78f4f4b2bd
poulouno <none@none.org>
2019-03-29 00:23:25 +0100

    fix is() return statement < instaed of <=

--- normalize.cpp
+++ normalize.cpp
@@ -3,14 +3,6 @@
 #include <type_traits>
 #include <iostream>
 
-enum class etoto:int{
-    eA=0,
-    eB=1,
-    eC=2,
-    eD=3,
-    eLast
-};
-
 
 template<typename E>
 constexpr auto to_integral(E e) -> typename std::underlying_type<E>::type 
@@ -19,24 +11,36 @@ constexpr auto to_integral(E e) -> typename std::underlying_type<E>::type
 }
 
 template<typename enum_T, typename out_T=int16_t>
-out_T normalize(enum_T e){
+constexpr out_T normalize(enum_T e){
     out_T outmax= std::numeric_limits<out_T>::max();
     return ((outmax+1)/(to_integral(enum_T::eLast)))*to_integral(e);
 }
 
 template<typename enum_T, typename in_T=int16_t>
-enum_T denormalize(in_T val){
+constexpr enum_T denormalize(in_T val){
     in_T inmax = std::numeric_limits<in_T>::max(); 
     in_T out =  val/((inmax+1)/(to_integral(enum_T::eLast)));
-    return (etoto(out));
+    return (enum_T(out));
 }
 
 template<typename enum_T, typename in_T=int16_t>
-bool is(in_T val, enum_T e){
+constexpr bool is(in_T val, enum_T e){
     in_T inmax = std::numeric_limits<in_T>::max(); 
     in_T out =  val/((inmax+1)/(to_integral(enum_T::eLast)));
-    return (out>=to_integral(e) && (out<=to_integral(e)+1));
+    return (out>=to_integral(e) && (out<to_integral(e)+1));
 }
+
+
+
+
+////////////////////////////////////////////////////////
+enum class etoto:int{
+    eA=0,
+    eB=1,
+    eC=2,
+    eD=3,
+    eLast
+};
 
 int main(){
     etoto e= etoto::eD;
